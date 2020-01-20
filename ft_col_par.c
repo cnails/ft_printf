e1cd5093c23427f49e7556139118e7060d219ab3
Celeste Nails <cnails@at-j6.21-school.ru>
2020-01-20 16:09:21 +0300

    tagging release w.t.f.

--- ft_col_par.c
+++ ft_col_par.c
@@ -6,7 +6,7 @@
 /*   By: cnails <cnails@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/24 18:27:35 by cnails            #+#    #+#             */
-/*   Updated: 2020/01/19 20:59:12 by cnails           ###   ########.fr       */
+/*   Updated: 2020/01/20 16:08:48 by cnails           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -110,24 +110,44 @@ void	col_o(t_printf *a, long long int c)
 {
 	char	*tmp;
 	int		new;
+	char	*tes;
 	char	*p;
 
 	if (a->dot == 1)
 		a->dot = 2;
 	tmp = ft_itoa_base(c, 8, 'a');
+	if (a->sharp && ft_strcmp(tmp, "0"))
+	{
+		tes = tmp;
+		tmp = ft_strjoin("0", tes);
+		free(tes);
+	}
 	if (a->space_2 > ft_strlen(tmp))
 	{
+		// printf("here\n");
 		if (a->dot == 2)
+		{
+			if (a->space && a->space_2)
+				a->dot = 1;
 			collect(a, ft_strjoin(ft_strset('0', a->space_2 - ft_strlen(tmp)), tmp), a->space_2);
+		}
 		else
 			collect(a, ft_strjoin(ft_strset(' ', a->space_2 - ft_strlen(tmp)), tmp), a->space_2);
 	}
 	else
 	{
+		// printf("here\n");
+		if (a->space && a->space_2 && a->dot)
+		{
+			// printf("here\n");
+			a->dot = 1;
+			a->space_2 = a->space;
+		}
 		if (a->space_2)
 			a->space = a->space_2;
 		collect(a, tmp, ft_strlen(tmp));
 	}
+	free(tmp);
 }
 
 void	col_f(t_printf *a, long double ld)
--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: cnails <cnails@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/24 17:18:53 by cnails            #+#    #+#             */
-/*   Updated: 2020/01/19 20:52:50 by cnails           ###   ########.fr       */
+/*   Updated: 2020/01/20 16:07:24 by cnails           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ void	col_space(t_printf *a, char *str, size_t len)
 	else
 		p = ft_strset(' ', a->space - len);
 	tmp = ft_strjoin(a->buf, p);
-	// printf("tmp = %s}\n", tmp);
 	free(a->buf);
+	free(p);
 	a->buf = tmp;
 	a->len += a->space - len;
 	a->space = 0;
@@ -37,27 +37,21 @@ void	col_space(t_printf *a, char *str, size_t len)
 void		collect(t_printf *a, char *str, size_t len)
 {
 	char *tmp;
+	char *new;
 
 	if (!a->buf)
 		a->buf = ft_strnew(1);
-	// printf("%s\n", str);
-	// printf("%d\n", len);
 	if (a->space > len && (!a->align || a->dot) && a->space > 0)
-	{
-		// printf("col_space = %d\n", a->space);
 		col_space(a, str, len);
-	}
 	tmp = ft_strsub(str, 0, len);
-	tmp = ft_strjoin(a->buf, tmp);
-	// printf("tmp = %s\n", tmp);
+	new = ft_strjoin(a->buf, tmp);
+	free(tmp);
+	tmp = new;
 	free(a->buf);
 	a->buf = tmp;
 	a->len += len;
 	if (a->space < 0 && -a->space > len)
-	{
-		// printf("%d\n", 10);
 		col_space(a, str, len);
-	}
 	a->space = 0;
 	a->dot = 0;
 	a->sharp = 0;
@@ -91,61 +85,17 @@ int			ft_printf(const char *str, ...)
         a.str++;
     }
 	write(1, a.buf, a.len);
+	free(a.buf);
 //	ft_putnbr(a.len);
 	va_end(a.va);
 	return (a.len);
 }
 
-// int 		main()
-// {
-// 	char *s;
+int 		main()
+{
+	char str[] = "0";
 
-// //	printf("%s", ft_strsub(t,0,s-t));
-// //	ft_printf("%.20f", 12345678901234567.123456789012345678901234567890);
-// //	printf("\n%     D", 11);
-// //	printf("\n%C", (-0 == 0) ? 'y' : 'n');
-// 	int c = 10000;
-// 	long long int a = 99999999999;
-// 	int o = 0;
-// 	// char *p = malloc(100);
-// 	// while ((c = ft_for(5, 1, 10)))
-// 	// while((c = ft_for(0, 3, 5)))
-// 		// ft_printf("%0d\n", c);
-
-// 	// printf("%x lol\n", c);
-
-
-// 	// printf("%c",3);
-// 	// printf("%llu\n", (unsigned long long)12345678901234567890.123456789012345678901234567890);
-// //	printf("%s", ft_dtoa(231.1234567890123456789, 3));
-// 	// printf("\n%.20f", 12345678901234567890.123456789012345678901234567890);
-// //	printf("%d\n",  (int)(0.12345));
-// //	printf("%.10Lf\n", (long double)12345678901234567890123.12345678901234567890);
-// //	printf( "%0*x", 8, 15 );
-// //	printf("\n**%2d",12345678);
-// //	printf("\n%3d %s", 12, "123");
-// // ft_printf("\n%.2f", 1234567890.1234567890);
-// 	// printf("%d\n", ft_printf("%p\n", p));
-// 	// printf("%d\n", ft_printf("%5d\n", 33));
-// 	// printf("{%ld}\n", (unsigned long int)((-2147483648)));
-// 	// ft_printf("{%ld}\n", (unsigned long int)(16383));
-// 	// ft_printf("{%ld}\n", (unsigned long int)(32767));
-// 	// printf("d = %x\n", ft_printf("{%x}\n"));
-// 	// printf("{%llu %hhu %llx}\n", 99999999999, "%llu%hhu%llx", "%llu%hhu%llx");
-// 	// ft_printf("%-3x\n", 0);
-// 	// printf("%-3x\n", 0);
-// 	ft_printf("%.5o\n", 21);
-// 	printf("%.5o\n", 21);
-// 	// ft_printf("%#05x\n", 43);
-// 	// printf("%#05x\n", 43);
-// //	printf("\n%d", -267);
-// 	// printf("{%hu}\n", "1");
-// 	// ft_printf("{%u}\n", (unsigned long long int)(-2147483649));
-// 	// printf("col = %d\n", ft_printf("%x\n", 4294967294 / 2));
-// 	// printf("col = %d\n", printf("%x\n", 4294967294));
-// //	printf("\n%.2f %s   %10d.\n", 1234567890.1234567890, "qwerty",10);
-// //	ft_printf("\n%.2f %s   %10d.", 1234567890.1234567890, "qwerty",10);
-
-// //	printf("\n%.s","qwertyuiop1234567");
-
-// }
+	// printf("%d\n", ft_strcmp(str, "a"));
+	ft_printf("%#5o\n", 43);
+	printf("%#5o\n", 43);
+}
