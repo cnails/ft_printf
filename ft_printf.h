21c27cb8cb757183c4b74b187111f33e1d1095ba
Donna Metallo <dmetallo@ga-o4.21-school.ru>
2020-01-24 16:19:36 +0300

    a

--- ft_printf.h
+++ ft_printf.h
@@ -15,7 +15,7 @@
 # include "libft/libft.h"
 # include <stdarg.h>
 
-typedef	struct	s_printf
+typedef	struct		s_printf
 {
 	size_t			len;
 	int				fd;
@@ -34,29 +34,29 @@ typedef	struct	s_printf
 	int				sharp;
 	int				one_s;
 	va_list			va;
-}				t_printf;
+}					t_printf;
 
-void		col_d(t_printf *a, void *nb);
-void		col_u(t_printf *a, void *nb, char c);
-void		col_s(t_printf *a, char *str);
-void		col_c(t_printf *a, char c);
-void		col_o(t_printf *a, long long int c);
-void		col_f(t_printf *a, long double ld);
-void		col_p(t_printf *a, void *str, char c);
-void		col_x(t_printf *a, void *str, char c);
-void		dot_space(t_printf *a);
-void		col_plus_min_sl(t_printf *a);
-void		col_hl(t_printf *a);
-void		col_par(t_printf *a);
-void		col_dot(t_printf *a);
-void		collect_space(t_printf *a);
-void		col_space(t_printf *a, char *str, size_t len);
-void		collect(t_printf *a, char *str, size_t len);
-void		dot_space(t_printf *a);
-long long	ret_nb(t_printf *a, void *nb);
-void		ft_printbits(unsigned long int octet);
-void		col_ld(t_printf *a, long int nb, char c);
-void		col_with_sign(t_printf *a, long long *l, char *sign);
-void		one_more_costil(t_printf *a, long long *l);
+void				col_d(t_printf *a, void *nb);
+void				col_u(t_printf *a, void *nb, char c);
+void				col_s(t_printf *a, char *str);
+void				col_c(t_printf *a, char c);
+void				col_o(t_printf *a, long long int c);
+void				col_f(t_printf *a, long double ld);
+void				col_p(t_printf *a, void *str, char c);
+void				col_x(t_printf *a, void *str, char c);
+void				dot_space(t_printf *a);
+void				col_plus_min_sl(t_printf *a);
+void				col_hl(t_printf *a);
+void				col_par(t_printf *a);
+void				col_dot(t_printf *a);
+void				collect_space(t_printf *a);
+void				col_space(t_printf *a, char *str, size_t len);
+void				collect(t_printf *a, char *str, size_t len);
+void				dot_space(t_printf *a);
+long long			ret_nb(t_printf *a, void *nb);
+void				ft_printbits(unsigned long int octet);
+void				col_ld(t_printf *a, long int nb, char c);
+void				col_with_sign(t_printf *a, long long *l, char *sign);
+void				one_more_costil(t_printf *a, long long *l);
 
 #endif
